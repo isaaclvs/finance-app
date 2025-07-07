f99f39f5835b83cc47fa935378f62f2e04467caf
Isaac <isaacfelipee3@gmail.com>
2025-07-07 15:40:29 -0300

    feat: configure RSpec and add model tests

---
+++ spec/factories/transactions.rb
@@ -0,0 +1,18 @@
+FactoryBot.define do
+  factory :transaction do
+    amount { Faker::Number.decimal(l_digits: 2, r_digits: 2) }
+    transaction_type { %w[income expense].sample }
+    date { Faker::Date.between(from: 30.days.ago, to: Date.current) }
+    description { Faker::Lorem.sentence }
+    association :user
+    association :category
+    
+    trait :income do
+      transaction_type { "income" }
+    end
+    
+    trait :expense do
+      transaction_type { "expense" }
+    end
+  end
+end
---
+++ spec/rails_helper.rb
@@ -0,0 +1,48 @@
+require 'spec_helper'
+ENV['RAILS_ENV'] ||= 'test'
+require_relative '../config/environment'
+abort("The Rails environment is running in production mode!") if Rails.env.production?
+require 'rspec/rails'
+require 'shoulda/matchers'
+require 'factory_bot_rails'
+require 'faker'
+require 'database_cleaner/active_record'
+
+Rails.root.glob('spec/support/**/*.rb').sort_by(&:to_s).each { |f| require f }
+
+begin
+  ActiveRecord::Migration.maintain_test_schema!
+rescue ActiveRecord::PendingMigrationError => e
+  abort e.to_s.strip
+end
+
+RSpec.configure do |config|
+  config.include FactoryBot::Syntax::Methods
+  
+  config.before(:suite) do
+    DatabaseCleaner.allow_remote_database_url = true
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+  
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
+  
+  config.fixture_paths = [
+    Rails.root.join('spec/fixtures')
+  ]
+
+  config.use_transactional_fixtures = false
+  config.infer_spec_type_from_file_location!
+  config.filter_rails_from_backtrace!
+end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
