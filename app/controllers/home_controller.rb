dbc819677acbb4756b3a7afa893f205f9d2ee11a
Isaac <isaacfelipee3@gmail.com>
2025-07-07 14:32:13 -0300

    feat: update home to redirect to transactions

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,7 @@ class HomeController < ApplicationController
   
   def index
     if user_signed_in?
-      redirect_to categories_path
+      redirect_to transactions_path
     else
       redirect_to new_user_session_path
     end
