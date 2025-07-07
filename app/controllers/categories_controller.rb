00e6c3e1d76af4f3d9f51662bed9e387fb7c409e
Isaac <isaacfelipee3@gmail.com>
2025-07-07 13:49:31 -0300

    feat: implement Categories controller with Turbo Stream support

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,62 @@
+class CategoriesController < ApplicationController
+  before_action :set_category, only: %i[ show edit update destroy ]
+  
+  def index
+    @categories = Current.user.categories.ordered
+  end
+  
+  def show
+    @recent_transactions = @category.transactions.recent
+  end
+  
+  def new
+    @category = Current.user.categories.build
+  end
+  
+  def edit
+  end
+  
+  def create
+    @category = Current.user.categories.build(category_params)
+    
+    if @category.save
+      respond_to do |format|
+        format.turbo_stream
+        format.html { redirect_to categories_path, notice: "Category was successfully created." }
+      end
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+  
+  def update
+    if @category.update(category_params)
+      respond_to do |format|
+        format.turbo_stream
+        format.html { redirect_to categories_path, notice: "Category was successfully updated." }
+      end
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+  
+  def destroy
+    if @category.destroy
+      respond_to do |format|
+        format.turbo_stream
+        format.html { redirect_to categories_path, notice: "Category was successfully deleted." }
+      end
+    else
+      redirect_to categories_path, alert: @category.errors.full_messages.join(", ")
+    end
+  end
+  
+  private
+    def set_category
+      @category = Current.user.categories.find(params[:id])
+    end
+    
+    def category_params
+      params.require(:category).permit(:name, :color)
+    end
+end
