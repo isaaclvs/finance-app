0c3b7fe24a40dfa4266358d274e99d12df9517d5
Isaac <isaacfelipee3@gmail.com>
2025-07-07 13:44:42 -0300

    feat: add Current model and Authentication concern

--- app/controllers/concerns/authentication.rb
+++ app/controllers/concerns/authentication.rb
@@ -1,13 +1,12 @@
 module Authentication
   extend ActiveSupport::Concern
-
+  
   included do
     before_action :set_current_user
   end
-
+  
   private
-
-  def set_current_user
-    Current.user = current_user
-  end
+    def set_current_user
+      Current.user = current_user
+    end
 end
