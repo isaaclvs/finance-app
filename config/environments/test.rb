98250fad22a2169d88aeb80feb11bc10451d1cde
Isaac <isaacfelipee3@gmail.com>
2025-07-07 15:44:39 -0300

    chore: configure test environment settings

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -51,3 +51,8 @@ Rails.application.configure do
   # Raise error when a before_action's only/except options reference missing actions.
   config.action_controller.raise_on_missing_callback_actions = true
 end
+
+# Allow any host in test environment
+Rails.application.configure do
+  config.hosts.clear
+end
