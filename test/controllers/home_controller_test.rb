adbd778d66b776a10555e655d8c3e58db6fffda1
Isaac <isaacfelipee3@gmail.com>
2025-07-14 17:58:40 -0300

    fix: configure Devise for tests and correct HomeController test expectations
    - Add Devise::Test::IntegrationHelpers to test_helper.rb for sign_in
    support
    - Update HomeController test to expect correct redirect behavior
    - Add test coverage for both logged in and logged out user scenarios

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,9 +1,14 @@
 require "test_helper"
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test "should redirect logged in user to transactions" do
     sign_in users(:one)
     get root_url
-    assert_response :success
+    assert_redirected_to transactions_path
+  end
+
+  test "should redirect logged out user to sign in" do
+    get root_url
+    assert_redirected_to new_user_session_path
   end
 end
