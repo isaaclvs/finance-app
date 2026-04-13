d10057cf814c3dc74e6300e6328783321846978a
Isaac Alves <isaacfelipee3@gmail.com>
2026-04-13 16:34:04 -0300

    feat: detect locale from browser preferences

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,44 +8,42 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   around_action :log_request_performance, if: :performance_logging_enabled?
 
-  def default_url_options
-    locale = I18n.locale
-    return {} if locale == I18n.default_locale
-
-    { locale: locale }
-  end
-
   private
-  def set_locale
-    if params[:locale].present? && I18n.available_locales.map(&:to_s).include?(params[:locale])
-      session[:locale] = params[:locale]
+    def set_locale
+      I18n.locale = extract_locale_from_header || I18n.default_locale
     end
 
-    locale = session[:locale].presence || I18n.default_locale
-    I18n.locale = locale
-  end
+    def extract_locale_from_header
+      header = request.env["HTTP_ACCEPT_LANGUAGE"].to_s
 
-  def performance_logging_enabled?
-    Rails.env.development? && %w[dashboard transactions].include?(controller_name)
-  end
+      locales = header.scan(/[A-Za-z-]+/).map(&:downcase)
+      return :"pt-BR" if locales.any? { |locale| locale == "pt-br" || locale.start_with?("pt") }
+      return :en if locales.any? { |locale| locale.start_with?("en") }
 
-  def log_request_performance
-    started_at = Process.clock_gettime(Process::CLOCK_MONOTONIC)
-    query_count = 0
-
-    callback = lambda do |_name, _started, _finished, _id, payload|
-      query_count += 1 if payload[:sql] !~ /SCHEMA/ && !payload[:cached]
+      nil
     end
 
-    ActiveSupport::Notifications.subscribed(callback, "sql.active_record") do
-      yield
+    def performance_logging_enabled?
+      Rails.env.development? && %w[dashboard transactions].include?(controller_name)
     end
 
-    duration_ms = ((Process.clock_gettime(Process::CLOCK_MONOTONIC) - started_at) * 1000).round(2)
+    def log_request_performance
+      started_at = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+      query_count = 0
 
-    Rails.logger.info(
-      "[perf] controller=#{controller_name} action=#{action_name} path=#{request.path} " \
-      "status=#{response.status} duration_ms=#{duration_ms} queries=#{query_count}"
-    )
-  end
+      callback = lambda do |_name, _started, _finished, _id, payload|
+        query_count += 1 if payload[:sql] !~ /SCHEMA/ && !payload[:cached]
+      end
+
+      ActiveSupport::Notifications.subscribed(callback, "sql.active_record") do
+        yield
+      end
+
+      duration_ms = ((Process.clock_gettime(Process::CLOCK_MONOTONIC) - started_at) * 1000).round(2)
+
+      Rails.logger.info(
+        "[perf] controller=#{controller_name} action=#{action_name} path=#{request.path} " \
+        "status=#{response.status} duration_ms=#{duration_ms} queries=#{query_count}"
+      )
+    end
 end
